6fc029d5275f9375b14072e1849547c47a1ea370
Hamza <h.nawabi007@gmail.com>
2022-09-17 10:51:36 +0430

    break

--- js.js
+++ js.js
@@ -26,3 +26,66 @@ for(let z in itemObj){
 }
 console.log('--------------')
 
+const text = `[
+    {
+      "userId": 1,
+      "firstName": "Krish",
+      "lastName": "Lee",
+      "phoneNumber": "123456",
+      "emailAddress": "krish.lee@learningcontainer.com",
+      "photo": "product-images/bike-0.jpeg"
+    },
+    {
+      "userId": 2,
+      "firstName": "fayaz",
+      "lastName": "Lee",
+      "phoneNumber": "123456",
+      "emailAddress": "krish.lee@learningcontainer.com",
+      "photo": "product-images/bike-1.jpeg"
+    },
+    {
+      "userId": 2,
+      "firstName": "asee",
+      "lastName": "Lee",
+      "phoneNumber": "123456",
+      "emailAddress": "krish.lee@learningcontainer.com",
+      "photo": "product-images/bike-2.jpeg"
+    },
+    {
+      "userId": 2,
+      "firstName": "narati",
+      "lastName": "Lee",
+      "phoneNumber": "123456",
+      "emailAddress": "krish.lee@learningcontainer.com",
+      "photo": "product-images/bike-3.jpeg"
+    },
+    {
+      "userId": 3,
+      "firstName": "rabin ",
+      "lastName": "Lee",
+      "phoneNumber": "123456",
+      "emailAddress": "krish.lee@learningcontainer.com",
+      "photo": "product-images/bike-4.jpeg"
+    } 
+  ]`;
+  const txtObj = JSON.parse(text)
+  console.log(txtObj[1])
+
+  var indata = document.querySelector('#datain')
+  var onj = ""
+  i=0
+  while(i<txtObj.length){
+    onj += `
+    <tr>
+    <td>${txtObj[i].userId}</td>
+    <td>${txtObj[i].firstName}</td>
+    <td>${txtObj[i].lastName}</td>
+    <td>${txtObj[i].phoneNumber}</td>
+    <td>${txtObj[i].emailAddress}</td>
+    <td><img style="height:50px;width:100px;" src="17_09_2022_json/product-images/bike-${[i]}.jpeg"></td>
+    <td><img style="height:50px;width:100px;" src="17_09_2022_json/${txtObj[i].photo}"></td>
+    </tr>
+    `
+    i++
+    indata.innerHTML=onj;
+  }
